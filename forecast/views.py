7a6f875783dc9aafa709be7bf50e7349416522b8
georgegyan <georgegyankwame72@gmail.com>
2025-04-28 20:43:18 +0000

    Initial commit: Django Weather Forecasting App setup with API integration

---
+++ forecast/views.py
@@ -0,0 +1,17 @@
+import requests
+from django.shortcuts import render
+
+def home(request):
+    weather_data = None
+
+    if request.method == 'POST':
+        city = request.POST.get('city')
+        api_key = '40707b12385b186a41bb1d3fb5445ac6'
+        url = f'https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={api_key}&units=metric'
+        response = requests.get(url)
+        if response.status_code == 200:
+            weather_data = response.json()
+        else:
+            weather_data = {'error': 'City not found or API error.'}
+
+    return render(request, 'forecast/home.html', {'weather_data': weather_data})
